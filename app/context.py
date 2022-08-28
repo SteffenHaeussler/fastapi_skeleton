9b462e3316a51fa7c81b3c211a8d7525911586fa
Steffen <steffen.haeussler.work@gmail.com>
2022-08-28 16:19:28 +0200

    black formatting

--- app/context.py
+++ app/context.py
@@ -1,5 +1,5 @@
 from contextvars import ContextVar
 
 
-ctx_request_id =  ContextVar('request_id', default="-")
-ctx_internal_id = ContextVar('internal_id', default="-")
+ctx_request_id = ContextVar("request_id", default="-")
+ctx_internal_id = ContextVar("internal_id", default="-")
