9dd60f2254b11494b0176919e337e45b94c177e6
Steffen <steffen.haeussler.work@gmail.com>
2026-05-09 14:41:16 +0200

    fix: align run_app coverage target

--- run_app.sh
+++ run_app.sh
@@ -3,7 +3,7 @@
 if [ "$FASTAPI_ENV" = "PROD" ]; then
 	uv run uvicorn src.app.main:app --host 0.0.0.0 --port 5000 --workers 2 --log-level "error"
 elif [ "$FASTAPI_ENV" = "TEST" ]; then
-	uv run pytest --cov-report html --cov=app tests
+	uv run pytest --cov-report html --cov=src/app tests
 else
 	 uv run uvicorn src.app.main:app --host 0.0.0.0 --port 5000 --workers 1 --log-level "debug"
 fi
